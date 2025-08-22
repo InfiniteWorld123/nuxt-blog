e6c1fc7db90863fd6689a83f205c81d7fcd57aa5
‪Yaman Warda‬‏ <Wardayaman47@gmail.com>
2025-08-22 23:57:50 +0200

    Add dynamic route for blog post and fix 404 error

---
+++ app/pages/post/index.vue
@@ -0,0 +1,9 @@
+<script setup>
+const { data: post, pending } = useContent();
+</script>
+
+<template>
+  <!-- <div>hello, this is the a post page</div> -->
+  <ContentRenderer v-if="post" :value="post" />
+  <div v-else>post not found</div>
+</template>
